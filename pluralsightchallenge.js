67aa35bf1eb2a5f93d4d290a92cea0ded86a80bf
Tiffany Fritz <fritzty@gmail.com>
2017-09-10 17:42:09 -0600

    split package from dependency

--- pluralsightchallenge.js
+++ pluralsightchallenge.js
@@ -1,15 +1,37 @@
-let validInput1 = ["KittenService: CamelCaser", "CamelCaser: "];
-let expectedOutput1 = "CamelCaser, KittenService";
-let validInput2 = ["KittenService: ",
-   "Leetmeme: Cyberportal",
-   "Cyberportal: Ice",
-   "CamelCaser: KittenService",
-   "Fraudstream: Leetmeme",
-   "Ice: "];
-let expectedOutput2 = "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream";
-let invalidInput1 = ["KittenService: ",
-   "Leetmeme: Cyberportal",
-   "Cyberportal: Ice"
-   "CamelCaser: KittenService",
-   "Fraudstream: ",
-   "Ice: Leetmeme"];
+
+// Starting with what I'm given...
+
+const validInput1 = ["KittenService: CamelCaser", "CamelCaser: "];
+const expectedOutput1 = "CamelCaser, KittenService";
+const validInput2 = [
+  "KittenService: ",
+  "Leetmeme: Cyberportal",
+  "Cyberportal: Ice",
+  "CamelCaser: KittenService",
+  "Fraudstream: Leetmeme",
+  "Ice: "
+];
+const expectedOutput2 = "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream";
+const invalidInput1 = [
+  "KittenService: ",
+  "Leetmeme: Cyberportal",
+  "Cyberportal: Ice",
+  "CamelCaser: KittenService",
+  "Fraudstream: ",
+  "Ice: Leetmeme"
+ ];
+
+// Format data so that the data is easier to work with
+
+function format(input) {
+  //split packages from dependencies
+  return input.reduce((accum, item) => {
+    const splitString = item.split(": ");
+    console.log(splitString);
+    const package = splitString[0];
+    const dependency = splitString[1];
+    return splitString;
+  }, {})
+}
+
+format(validInput2);
